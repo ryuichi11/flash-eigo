4d92f14fb883e11ddbbd68cc8a5b6dd7165ac751
ryuichi11 <ryuandst@icloud.com>
2026-07-23 02:59:09 -0700

    Add questions array with emojis and translations

---
+++ questions.js
@@ -0,0 +1,51 @@
+const questions = [
+
+{
+emoji:"🍎",
+english:"Apple",
+japanese:"りんご"
+},
+
+{
+emoji:"🐶",
+english:"Dog",
+japanese:"犬"
+},
+
+{
+emoji:"👦🏃",
+english:"He runs.",
+japanese:"彼は走ります。"
+},
+
+{
+emoji:"👧📚",
+english:"She reads a book.",
+japanese:"彼女は本を読みます。"
+},
+
+{
+emoji:"👦🍎🍽️",
+english:"He eats an apple.",
+japanese:"彼はりんごを食べます。"
+},
+
+{
+emoji:"👩🥤",
+english:"She drinks water.",
+japanese:"彼女は水を飲みます。"
+},
+
+{
+emoji:"👦🏫",
+english:"He goes to school.",
+japanese:"彼は学校へ行きます。"
+},
+
+{
+emoji:"🕒👦🏫",
+english:"Yesterday he went to school.",
+japanese:"昨日彼は学校へ行きました。"
+}
+
+];
